e2a74b5af81cb79489f9bc98abd88b1f92570828
duckky93 <kimkhoa20793@gmail.com>
2016-06-21 09:02:59 +0700

    fix bug Kyler_BUG_021

--- Kyler/MusicPlayer/app/src/main/java/com/example/kyler/musicplayer/View/MainActivity.java
+++ Kyler/MusicPlayer/app/src/main/java/com/example/kyler/musicplayer/View/MainActivity.java
@@ -14,6 +14,7 @@ import com.example.kyler.musicplayer.View.Fragment.ListSongFragment;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button mButtonListSong,mButtonFavoriteSong,mButtonFilter;
+    final int LISTSONGID = 0, FAVORITESONGID = 1, FILTER = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +24,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mButtonFavoriteSong = (Button) findViewById(R.id.activity_main_bt_favorite_song);
         mButtonFilter = (Button) findViewById(R.id.activity_main_bt_filter);
         mButtonListSong.setOnClickListener(this);
+        openFragment(LISTSONGID);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.activity_main_bt_list_song:
-                Fragment fragment = new ListSongFragment();
-                openFragment(fragment);
+                openFragment(LISTSONGID);
                 break;
             case R.id.activity_main_bt_favorite_song:
                 break;
@@ -39,7 +40,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public void openFragment(android.app.Fragment fragment){
+    public void openFragment(int id){
+        Fragment fragment = null;
+        switch (id){
+            case LISTSONGID:
+                fragment = new ListSongFragment();
+                break;
+            case FAVORITESONGID:
+                break;
+            case FILTER:
+                break;
+            default : break;
+        }
         if(fragment != null){
             FragmentManager fragmentManager = this.getFragmentManager();
             fragmentManager.beginTransaction().replace(R.id.main_frame,fragment).commit();

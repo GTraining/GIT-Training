52e667bbd16518bf2e9c6beb2cb2645276b86528
nhathoang1007 <nhathoang407@gmail.com>
2016-06-23 10:02:29 +0700

    Commit Task ID - JS_022

--- Jason/Jason-WorkShop-3/app/src/main/java/com/example/jason/jason_workshop_3/View/LoginView/LoginActivity.java
+++ Jason/Jason-WorkShop-3/app/src/main/java/com/example/jason/jason_workshop_3/View/LoginView/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.jason.jason_workshop_3.View.LoginView;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 

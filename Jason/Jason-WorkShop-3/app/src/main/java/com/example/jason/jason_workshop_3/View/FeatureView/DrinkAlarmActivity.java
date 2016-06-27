3ae36e16d627f9817bc15a6ed75c627181fd2529
nhathoang1007 <nhathoang407@gmail.com>
2016-06-27 17:38:46 +0700

    JS_021

---
+++ Jason/Jason-WorkShop-3/app/src/main/java/com/example/jason/jason_workshop_3/View/FeatureView/DrinkAlarmActivity.java
@@ -0,0 +1,15 @@
+package com.example.jason.jason_workshop_3.View.FeatureView;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jason.jason_workshop_3.R;
+
+public class DrinkAlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_alarm);
+    }
+}
